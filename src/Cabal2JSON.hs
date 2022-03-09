5b5e6fc6135db7ff57e6ecb78a7acbea15ab6835
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-03-09 19:23:48 +0100

    undefineds to fill in

--- src/Cabal2JSON.hs
+++ src/Cabal2JSON.hs
@@ -1,14 +1,92 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Cabal2JSON
   ( cabal2JSON,
   )
 where
 
+import Autodocodec
+import Control.Arrow (first)
+import qualified Data.HashMap.Strict as HM
+import Data.Hashable
 import Distribution.PackageDescription.Parsec as Cabal
+import Distribution.Types.Benchmark as Cabal
+import Distribution.Types.CondTree as Cabal
+import Distribution.Types.ConfVar as Cabal
+import Distribution.Types.Dependency as Cabal
+import Distribution.Types.Executable as Cabal
+import Distribution.Types.Flag as Cabal
+import Distribution.Types.ForeignLib as Cabal
+import Distribution.Types.GenericPackageDescription as Cabal
+import Distribution.Types.Library as Cabal
+import Distribution.Types.PackageDescription as Cabal
+import Distribution.Types.TestSuite as Cabal
+import Distribution.Types.UnqualComponentName as Cabal
 import Distribution.Verbosity as Cabal
 import System.Environment
+import Text.Show.Pretty
 
 cabal2JSON :: IO ()
 cabal2JSON = do
   arg : _ <- getArgs
-  packageDescription <- Cabal.readGenericPackageDescription Cabal.deafening arg
-  print packageDescription
+  cabalFile <- Cabal.readGenericPackageDescription Cabal.deafening arg
+  pPrint cabalFile
+
+instance HasCodec GenericPackageDescription where
+  codec =
+    object "GenericPackageDescription" $
+      GenericPackageDescription
+        <$> requiredField' "description" .= packageDescription
+        <*> optionalFieldWithOmittedDefault' "package-flags" [] .= genPackageFlags
+        <*> optionalField' "library" .= condLibrary
+        <*> optionalFieldWithOmittedDefaultWith' "sublibraries" unqualComponentNameCodec [] .= condSubLibraries
+        <*> optionalFieldWithOmittedDefaultWith' "foreign-libs" unqualComponentNameCodec [] .= condForeignLibs
+        <*> optionalFieldWithOmittedDefaultWith' "executables" unqualComponentNameCodec [] .= condExecutables
+        <*> optionalFieldWithOmittedDefaultWith' "test-suites" unqualComponentNameCodec [] .= condTestSuites
+        <*> optionalFieldWithOmittedDefaultWith' "benchmarks" unqualComponentNameCodec [] .= condBenchmarks
+
+unqualComponentNameCodec :: HasCodec b => JSONCodec [(UnqualComponentName, b)]
+unqualComponentNameCodec = mapInListForCodec mkUnqualComponentName unUnqualComponentName
+
+mapInListForCodec ::
+  HasCodec b =>
+  (String -> a) ->
+  (a -> String) ->
+  JSONCodec [(a, b)]
+mapInListForCodec fromText toText =
+  dimapCodec
+    (f . HM.toList)
+    (HM.fromList . g)
+    codec
+  where
+    f = map (first fromText)
+    g = map (first toText)
+
+instance Hashable UnqualComponentName where
+  hashWithSalt s = hashWithSalt s . unUnqualComponentName
+
+instance HasCodec PackageDescription where
+  codec = undefined
+
+instance HasCodec Flag where
+  codec = undefined
+
+instance HasCodec a => HasCodec (CondTree ConfVar [Dependency] a) where
+  codec = undefined
+
+instance HasCodec Library where
+  codec = undefined
+
+instance HasCodec ForeignLib where
+  codec = undefined
+
+instance HasCodec Executable where
+  codec = undefined
+
+instance HasCodec TestSuite where
+  codec = undefined
+
+instance HasCodec Benchmark where
+  codec = undefined
